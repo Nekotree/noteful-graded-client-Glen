59a473c0cbd1137d17c00112667b3a7dea381a64
Alex Larrabee <nekotree556@gmail.com>
2020-04-25 14:04:35 -0700

    first commit

---
+++ src/api-config.js
@@ -0,0 +1,6 @@
+const api_config = {
+  folders: 'https://sheltered-coast-32060.herokuapp.com/api/folders',
+  notes: 'https://sheltered-coast-32060.herokuapp.com/api/notes'
+};
+
+export default api_config;
